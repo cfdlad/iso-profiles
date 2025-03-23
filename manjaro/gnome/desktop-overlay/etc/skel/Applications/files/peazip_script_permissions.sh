a0cc244955b06fccbe5fd50600bc6461fb4b054d
cfdlad <babangs00@gmail.com>
2025-03-23 23:11:57 +0000

    Added march 23 2025 updates

---
+++ manjaro/gnome/desktop-overlay/etc/skel/Applications/files/peazip_script_permissions.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+##### Peazip
+chmod a+x ~/.local/share/nautilus/scripts/'Add to Archive'
+chmod a+x ~/.local/share/nautilus/scripts/Convert
+chmod a+x ~/.local/share/nautilus/scripts/'Extract Archive'
+chmod a+x ~/.local/share/nautilus/scripts/'Extract Here'
+chmod a+x ~/.local/share/nautilus/scripts/'Extract to Folder'
+chmod a+x ~/.local/share/nautilus/scripts/'Open Archive'
+
---
+++ manjaro/gnome/desktop-overlay/etc/skel/Applications/files/remove_vulkan-radeon.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+#Pamac remove 
+yes | sudo pamac remove lib32-vulkan-radeon  vulkan-radeon
+
